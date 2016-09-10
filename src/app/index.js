279cc079dfafdaaf375f68e5989a753c439c5704
Artem Devlysh <artem@devlysh.com>
2016-09-10 21:38:55 +0300

    env(create-env): Create development environment
    This is intermediate commit
    
    Closes #1

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,14 @@ var template = require('./index.html');
 var style = require('./index.less');
 
 function App () {
+    this.template = document.createElement('div');
+    this.template.innerHTML = template;
+    this.template.classList.add('app');
+    this.style = document.createElement('style');
+    this.style.innerHTML = style;
+
+    document.body.appendChild(this.template);
+    document.head.appendChild(this.style);
 }
 
 module.exports = new App();
